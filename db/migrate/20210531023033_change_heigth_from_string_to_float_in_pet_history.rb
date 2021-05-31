a992178eefc1ba1ebac7c7d9b44fcb380bedbe15
Danielid Ramirez <danielid132@gmail.com>
2021-05-30 22:52:24 -0400

    Modifying migration

--- db/migrate/20210531023033_change_heigth_from_string_to_float_in_pet_history.rb
+++ db/migrate/20210531023033_change_heigth_from_string_to_float_in_pet_history.rb
@@ -2,7 +2,7 @@ class ChangeHeigthFromStringToFloatInPetHistory < ActiveRecord::Migration[5.2]
   def change
     reversible do |dir|
       change_table :pet_histories do |t|
-        dir.up { t.change :heigth, :double }
+        dir.up { t.change :heigth, :float }
         dir.down { t.change :heigth, :string }
       end
     end
